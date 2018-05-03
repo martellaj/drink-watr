852b82f08769035c83d303b4cc512d7f1ea838db
Joe Martella <martellaj@live.com>
2018-05-02 21:52:27 -0700

    Simplifies loop to just run every hour (closes #1)

--- src/App.js
+++ src/App.js
@@ -6,23 +6,11 @@ class App extends Component {
         if (Notification.permission !== 'granted') {
             Notification.requestPermission();
         } else {
-            // Set initial hour (so we don't instantly get notification).
-            let lastNotifiedHour = new Date().getHours();
-
-            // Run notification loop every 15 minutes.
+            // Run notification loop every hour.
             setInterval(() => {
-                // Get current hour.
-                const currentHour = new Date().getHours();
-
-                // If hour is different, send a notification and reset hour.
-                if (lastNotifiedHour !== currentHour) {
-                    // Send notification.
-                    this.onNotify();
-
-                    // Set new hour.
-                    lastNotifiedHour = currentHour;
-                }
-            }, 900000 /* 15 minutes */);
+                // Send notification.
+                this.onNotify();
+            }, 3600000 /* 1 hour */);
         }
     }
 
